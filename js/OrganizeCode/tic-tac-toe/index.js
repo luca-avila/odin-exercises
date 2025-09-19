d2c6ffbba3d143fc8318c3e428bcad00c7dd2889
luca-avila <avilaluca61@gmail.com>
2025-09-19 14:39:08 -0300

    [tic-tac-toe] feat: Add hidden state UI handler function

--- js/OrganizeCode/tic-tac-toe/index.js
+++ js/OrganizeCode/tic-tac-toe/index.js
@@ -1,6 +1,6 @@
 import { createGameboard } from './ui/gameboard.js';
 import { createCellClickListener } from './events/listeners.js';
-import { createNamesInterface, displayPlayerData, resetBoardButton, updateScore } from './ui/interface.js';
+import { createNamesInterface, displayPlayerData, resetBoardButton, updateScore, toggleHiddenState } from './ui/interface.js';
 
 const gameboard = createGameboard();
 
@@ -10,8 +10,13 @@ document.body.appendChild(player2Data);
 document.body.appendChild(player1Data);
 const resetBtn = resetBoardButton();
 document.body.appendChild(resetBtn);
-
 document.body.appendChild(gameboard);
+
+const namesInterface = createNamesInterface();
+document.body.appendChild(namesInterface);
+
+const startButton = document.querySelector('.start-button');
+startButton.addEventListener('click', handleStartButtonClick);
 
 const cells = document.querySelectorAll('.cell');
 cells.forEach(cell => createCellClickListener(cell));
