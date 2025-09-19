8a43cd842f2d4bb476af00038b384d872f5b50ee
luca-avila <avilaluca61@gmail.com>
2025-09-19 12:53:18 -0300

    [tic-tac-toe] fix: Change function names and class names to correctly represent columns and rows in the dom

--- js/OrganizeCode/tic-tac-toe/index.js
+++ js/OrganizeCode/tic-tac-toe/index.js
@@ -1,5 +1,9 @@
 import { createGameboard } from './ui/gameboard.js';
+import { createCellClickListener } from './events/listeners.js';
 
 const gameboard = createGameboard();
 
 document.body.appendChild(gameboard);
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => createCellClickListener(cell));
--- js/OrganizeCode/tic-tac-toe/ui/gameboard.js
+++ js/OrganizeCode/tic-tac-toe/ui/gameboard.js
@@ -2,24 +2,24 @@ function createGameboard() {
     const gameboard = document.createElement('div');
     gameboard.id = 'game-board';
 
-    gameboard.appendChild(createFirstRow());
-    gameboard.appendChild(createSecondRow());
-    gameboard.appendChild(createThirdRow());
+    gameboard.appendChild(createFirstColumn());
+    gameboard.appendChild(createSecondColumn());
+    gameboard.appendChild(createThirdColumn());
 
     return gameboard;
 }
 
-function createFirstRow() {
+function createFirstColumn() {
     const firstRow = document.createElement('div');
-    firstRow.classList.add('first-row');
+    firstRow.classList.add('first-column');
 
     const cell1 = createCell();
     const cell2 = createCell();
     const cell3 = createCell();
 
-    cell1.classList.add('col-1');
-    cell2.classList.add('col-2');
-    cell3.classList.add('col-3');
+    cell1.classList.add('row-1');
+    cell2.classList.add('row-2');
+    cell3.classList.add('row-3');
 
     firstRow.appendChild(cell1);
     firstRow.appendChild(cell2);
@@ -28,17 +28,17 @@ function createFirstRow() {
     return firstRow;
 }
 
-function createSecondRow() {
+function createSecondColumn() {
     const secondRow = document.createElement('div');    
-    secondRow.classList.add('second-row');
+    secondRow.classList.add('second-column');
     
     const cell1 = createCell();
     const cell2 = createCell();
     const cell3 = createCell();
 
-    cell1.classList.add('col-1');
-    cell2.classList.add('col-2');
-    cell3.classList.add('col-3');
+    cell1.classList.add('row-1');
+    cell2.classList.add('row-2');
+    cell3.classList.add('row-3');
 
     secondRow.appendChild(cell1);
     secondRow.appendChild(cell2);
@@ -47,17 +47,17 @@ function createSecondRow() {
     return secondRow;
 }
 
-function createThirdRow() {
+function createThirdColumn() {
     const thirdRow = document.createElement('div');
-    thirdRow.classList.add('third-row');
+    thirdRow.classList.add('third-column');
 
     const cell1 = createCell();
     const cell2 = createCell();
     const cell3 = createCell();
 
-    cell1.classList.add('col-1');
-    cell2.classList.add('col-2');
-    cell3.classList.add('col-3');
+    cell1.classList.add('row-1');
+    cell2.classList.add('row-2');
+    cell3.classList.add('row-3');
 
     thirdRow.appendChild(cell1);
     thirdRow.appendChild(cell2);
