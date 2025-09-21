a463aee46407794dbffe69d6a7ece75b0fcb4bc9
luca-avila <avilaluca61@gmail.com>
2025-09-21 16:40:27 -0300

    [library] refactor: Change book constructor and prototype for class syntax

--- js/OrganizeCode/library/models/book.js
+++ js/OrganizeCode/library/models/book.js
@@ -1,27 +1,61 @@
-function Book (name, author, pages, read = false) {
+class Book {
+    #id;
+    #name;
+    #author;
+    #pages;
+    #read;
 
-    if (!new.target) {
-        throw new Error('Use the new keyword to create a new Book instance');
-    }
-    if (typeof name !== 'string' || typeof author !== 'string') {
-        throw new Error('Name and author must be strings');
-    }
-    if (pages && typeof pages !== 'number') {
-        throw new Error('Pages must be a number');
-    }
-    if (typeof read !== 'boolean') {
-        throw new Error('Read must be a boolean');
+    constructor (name, author, pages, read = false){
+
+        this.generateId();
+        this.setName(name);
+        this.setAuthor(author);
+        this.setPages(pages);
+        this.setRead(read);
     }
 
-    this.id = crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).substring(2, 11);
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+    generateId() {
+        this.#id = crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).substring(2, 11);
+    }
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+    getName() {return this.#name;}
+    setName(name) {
+        if (typeof name !== 'string') {
+            throw new Error('Name and author must be strings');
+        }
+        this.#name = name;
+    }
+
+    getAuthor() {return this.#author;}
+    setAuthor(author) {
+        if (typeof author !== 'string') {
+            throw new Error('Author must be a string');
+        }
+        this.#author = author;
+    }
+
+    getPages() {return this.#pages;}
+    setPages(pages) {
+        if (pages && typeof pages !== 'number') {
+            throw new Error('Pages must be a number');
+        }
+        this.#pages = pages;
+    }
+
+    getRead() {return this.#read;}
+    setRead(read) {
+        if (typeof read !== 'boolean') {
+            throw new Error('Read must be a boolean');
+        }
+        this.#read = read;
+    }
+
+    getId() {return this.#id;}
+
+    toggleRead() {
+        this.#read = !this.#read;
+    }
+    
 }
 
 export { Book };
