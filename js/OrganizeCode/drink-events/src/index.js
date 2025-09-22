e1f140339fc427c11b8c659231929b7d5f890317
luca-avila <avilaluca61@gmail.com>
2025-09-22 18:01:49 -0300

    [drink-events] feat: Add total current requirements for the project

---
+++ js/OrganizeCode/drink-events/src/index.js
@@ -0,0 +1,1 @@
+console.log('Webpack is working! Drink Events website loaded successfully!');
--- js/OrganizeCode/drink-events/webpack.config.js
+++ js/OrganizeCode/drink-events/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,9 +9,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    watchFiles: ['./src/template.html'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/template.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
 };
