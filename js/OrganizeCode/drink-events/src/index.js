fd3dc0c81b0790080cfc701a7a52146279b08031
luca-avila <avilaluca61@gmail.com>
2025-09-22 22:58:06 -0300

    [drink-events] feat(Webpack): support local fonts and images

--- js/OrganizeCode/drink-events/src/index.js
+++ js/OrganizeCode/drink-events/src/index.js
@@ -1,1 +1,6 @@
 console.log('Webpack is working! Drink Events website loaded successfully!');
+import { createHome } from './ui/home.js';
+
+const home = createHome();
+
+document.body.appendChild(home);
