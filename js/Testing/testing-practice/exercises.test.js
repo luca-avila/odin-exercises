812049784aa041c3e37c62f7fa82103a69f61478
luca-avila <avilaluca61@gmail.com>
2025-10-02 15:30:33 -0300

    [testing-practice] Add calculator sum, add, divide and multiply test

--- js/Testing/testing-practice/exercises.test.js
+++ js/Testing/testing-practice/exercises.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './exercises';
+import { capitalize, reverseString, calculator } from './exercises';
 
 test('capitalize test', () => {
     expect(capitalize('test')).toBe('Test');
@@ -6,4 +6,20 @@ test('capitalize test', () => {
 
 test('reverse string hello world!', () => {
     expect(reverseString('Hello World!')).toBe('!dlroW olleH');
+});
+
+test('calculator add 1 + 2', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('calculator subtract 5 - 3', () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
+});
+
+test('calculator multiply  7 * 3', () => {
+    expect(calculator.multiply(7, 3)).toBe(21);
+});
+
+test('calculator divide 15 / 3', () => {
+    expect(calculator.divide(15, 3)).toBe(5);
 });
